265c2c584139badd9b4ac5183cddeb76ab0678a6
jmorrow30 <96271225+jmorrow30@users.noreply.github.com>
2021-12-31 15:39:45 +0000

    Update 4.py

--- 4.py
+++ 4.py
@@ -6,6 +6,7 @@ C=[1 0];
 D=[0 0];
 x_eq=[[20];[0.4]];
 u_eq=[[0.672];[0.5]];
+t=0:0.00001:0.02;
 u_loop=0;
 u1=0.672*(ones(length(t),1));
 u2=0.5*(ones(length(t),1));
@@ -17,20 +18,14 @@ if u_loop<1
 end
 if u_loop>0
     u=u2;
+    figure
 end
-disp('u')
-disp(u(1))
-
 [num,den]=ss2tf(A,B,C,D,1);
 sys1=tf(num,den);
-t=0:0.00001:0.02;
-length(t);
-
 sys_ss1=ss(sys1);
 
 y1=lsim(sys1,u,t);
 u_loop=u_loop+1;
-figure
 A_new=[[-1818.1 -935.3];[-352.5 -2422.5]];
 [num,den]=ss2tf(A_new,B,C,D,1);
 sys2=tf(num,den);
