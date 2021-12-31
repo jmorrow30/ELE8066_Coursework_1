3b275d144fd22a2316e1a98537f52eb37870eb56
jmorrow30 <96271225+jmorrow30@users.noreply.github.com>
2021-12-31 15:28:10 +0000

    Update 4.py

--- 4.py
+++ 4.py
@@ -6,27 +6,59 @@ C=[1 0];
 D=[0 0];
 x_eq=[[20];[0.4]];
 u_eq=[[0.672];[0.5]];
+u_loop=0;
+u1=0.672*(ones(length(t),1));
+u2=0.5*(ones(length(t),1));
+
+while u_loop<3
+
+if u_loop<1
+    u=u1;
+end
+if u_loop>0
+    u=u2;
+end
+disp('u')
+disp(u(1))
+
 [num,den]=ss2tf(A,B,C,D,1);
 sys1=tf(num,den);
 t=0:0.00001:0.02;
 length(t);
-u=0.5*(ones(length(t),1));
+
 sys_ss1=ss(sys1);
+
 y1=lsim(sys1,u,t);
-
-
+u_loop=u_loop+1;
 figure
 A_new=[[-1818.1 -935.3];[-352.5 -2422.5]];
 [num,den]=ss2tf(A_new,B,C,D,1);
 sys2=tf(num,den);
 sys_ss2=ss(sys2);
-y2=lsim(sys,u,t);
+y2=lsim(sys2,u,t);
 
 plot(t,y1,t,y2)
 legend('Open loop','Closed loop')
 xlabel('Time (seconds)')
 ylabel('Amplitude')
+if u==0.672
+    title('y against t for u = u1 = 0.672')
+end
+if u==0.5
+    title('y against t for u = u2 = 0.5')
+end
 
 figure
-step(sys_ss1,sys_ss2)
-legend('Open loop','Closed loop')
+step(sys_ss1,sys_ss2);
+if u==0.672
+    title('Step response for u = u1 = 0.672')
+    legend('Open loop for u1=0.672','Closed loop for u1=0.672')
+end
+if u==0.5
+    title('Step response for u = u2 = 0.5')
+    legend('Open loop for u2=0.5','Closed loop for u2=0.5')
+end
+
+u_loop=u_loop+1;
+
+end
