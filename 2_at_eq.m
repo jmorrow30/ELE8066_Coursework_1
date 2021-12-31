186fa35b5002574f10510b922b6e575bbbe390de
jmorrow30 <96271225+jmorrow30@users.noreply.github.com>
2021-12-31 19:50:46 +0000

    Update 2_at_eq.m

--- 2_at_eq.m
+++ 2_at_eq.m
@@ -3,19 +3,15 @@ clear
 r = 1; % initializations
 L = 1;
 perturbation=1*rand(1,2);
-X = [1 0]+perturbation;
+%X = [1 0]+perturbation;
+X=[20 0.4]+perturbation;
+x_eq=[[20];[0.4]];
+u_eq=[[0.672];[0.5]];
 U = [];
 Z = [];
 Time = [0];
-u=[0.5];
-u1 = [0.672];
-u2 = 0.5;
-L = 200*10^-6;    %200μH
-R = [0.2];    %RL=0.2Ω
-vs = [15];    %15V
-c = [22*10^-6];        %C=22μF
-I_load = [0.2];       %Iload=0.2A
-tinterval=1;
+u=[0.97478];
+tinterval=5;
 
 %the equilibrium point is [1;0], and with input ueq=0.97478
 
@@ -23,7 +19,7 @@ hold on
 for i=1:length(u)
     tspan = tinterval*[(i-1) i];
     x0 = X(end,:);
-    [t,x]=ode45(@(t,x) nonlinear_example(t,x,u(i)),tspan,x0);
+   [t,x]=ode45(@(t,x) nonlinear_example(t,x,u(i)),tspan,x0);
     X=[X;x];
     U=[U; u(i)*ones(length(t),1)];
     Time=[Time;t];
@@ -60,7 +56,7 @@ tgrid=linspace(0,5,N)
 
 y_eq=0.84147
 ylin=ylin+y_eq
- 
+
 %figure
 plot(Y,'LineWidth',2)
 hold on
@@ -80,24 +76,13 @@ plot(tlin,xlin(:,2),'LineWidth',2)
 title('nonlinear vs liearized state 2 response')
 
 
-
-
 function dxdt = nonlinear_example(~,x,u)
 
-%dxdt=[0 (u2/c);(-u2/L) (-R/L)]*[x1;x2]+[0 0;(vs/L) 0]*[u1;u2]+[(-I_load/c);0]
 dxdt=zeros(2,1);
-%dxdt(1) = ((u2*x1)/c)+((I_load)/c);
-%dxdt(2) = (((-u2*x1)/L)+((-R*x2)/L))+((vs*u1)/L);
-dxdt(1) = ((0.5*x(1))/(22*10^-6))+((0.2)/(22*10^-6));
-dxdt(2) = (((-0.5*x(1))/(200*10^-6))+((-0.2*x(2))/(200*10^-6)))+((15*0.672)/(200*10^-6));
+%dxdt(1) = 0.5*(x(2)+sin(x(2)));
+%dxdt(2) = 8*(1-x(1))*x(1)-6*x(2)+0.804*u-exp(-0.25*u);
+
+dxdt(1) = u*(x(1))/(22)+((0.2)/(22));
+dxdt(2) = (((-u*x(1))/(200))+((-0.2*x(2))/(200)))+((15*u)/(200));
 
 end
-
-
-%u1 = [0.672];
-%u2 = 0.5;
-%L = 200*10^-6;    %200μH
-%R = [0.2];    %RL=0.2Ω
-%vs = [15];    %15V
-%c = [22*10^-6];        %C=22μF
-%I_load = [0.2];       %Iload=0.2A
